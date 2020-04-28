b8f9313c5446ac8ee14da82c84c0a24bd50f6759
Jairo R <jariver92@gmail.com>
2020-04-28 21:55:35 +0200

    firs commit created routes

---
+++ utils/mocks/movies.js
@@ -0,0 +1,142 @@
+const moviesMock =
+    [
+        {
+            "id": "6339aebf-23c6-4210-8ad3-ff557e7b6830",
+            "title": "King Kong",
+            "year": 2008,
+            "cover": "http://dummyimage.com/161x244.jpg/cc0000/ffffff",
+            "description": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+            "contentRating": null,
+            "source": "http://patch.com/odio/elementum/eu.js?nisi=consequat&eu=dui&orci=nec&mauris=nisi&lacinia=volutpat&sapien=eleifend&quis=donec&libero=ut&nullam=dolor&sit=morbi&amet=vel&turpis=lectus&elementum=in&ligula=quam&vehicula=fringilla&consequat=rhoncus&morbi=mauris&a=enim&ipsum=leo&integer=rhoncus&a=sed&nibh=vestibulum&in=sit&quis=amet&justo=cursus&maecenas=id&rhoncus=turpis&aliquam=integer&lacus=aliquet&morbi=massa&quis=id&tortor=lobortis&id=convallis&nulla=tortor&ultrices=risus&aliquet=dapibus&maecenas=augue&leo=vel&odio=accumsan&condimentum=tellus&id=nisi&luctus=eu&nec=orci&molestie=mauris&sed=lacinia&justo=sapien&pellentesque=quis&viverra=libero&pede=nullam&ac=sit&diam=amet&cras=turpis&pellentesque=elementum&volutpat=ligula&dui=vehicula&maecenas=consequat&tristique=morbi&est=a&et=ipsum&tempus=integer&semper=a&est=nibh&quam=in&pharetra=quis&magna=justo&ac=maecenas&consequat=rhoncus",
+            "tags": [
+                "Drama"
+            ]
+        },
+        {
+            "id": "665f96ae-c3f6-4760-a347-6de30e132934",
+            "title": "Spread",
+            "year": 2000,
+            "cover": "http://dummyimage.com/131x153.png/cc0000/ffffff",
+            "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+            "contentRating": null,
+            "source": "https://reddit.com/eleifend/donec/ut/dolor.js?ante=eget&ipsum=eros&primis=elementum&in=pellentesque&faucibus=quisque&orci=porta&luctus=volutpat&et=erat&ultrices=quisque&posuere=erat&cubilia=eros&curae=viverra&duis=eget&faucibus=congue&accumsan=eget&odio=semper&curabitur=rutrum&convallis=nulla&duis=nunc&consequat=purus&dui=phasellus&nec=in&nisi=felis&volutpat=donec&eleifend=semper&donec=sapien&ut=a&dolor=libero&morbi=nam&vel=dui&lectus=proin&in=leo&quam=odio&fringilla=porttitor&rhoncus=id&mauris=consequat&enim=in&leo=consequat&rhoncus=ut&sed=nulla&vestibulum=sed&sit=accumsan&amet=felis&cursus=ut&id=at&turpis=dolor&integer=quis&aliquet=odio&massa=consequat&id=varius&lobortis=integer&convallis=ac&tortor=leo&risus=pellentesque&dapibus=ultrices&augue=mattis&vel=odio&accumsan=donec&tellus=vitae&nisi=nisi&eu=nam&orci=ultrices&mauris=libero&lacinia=non&sapien=mattis&quis=pulvinar&libero=nulla&nullam=pede&sit=ullamcorper&amet=augue&turpis=a&elementum=suscipit&ligula=nulla&vehicula=elit&consequat=ac&morbi=nulla&a=sed&ipsum=vel&integer=enim&a=sit&nibh=amet&in=nunc&quis=viverra&justo=dapibus&maecenas=nulla&rhoncus=suscipit&aliquam=ligula&lacus=in&morbi=lacus&quis=curabitur",
+            "tags": [
+                "Comedy|Drama",
+                "Mystery|Thriller",
+                "Mystery|Thriller"
+            ]
+        },
+        {
+            "id": "262e1823-7161-4b47-bd73-146d899b5312",
+            "title": "Scotland, Pa.",
+            "year": 1999,
+            "cover": "http://dummyimage.com/240x166.jpg/5fa2dd/ffffff",
+            "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+            "contentRating": null,
+            "source": "http://hao123.com/ante/ipsum/primis/in.jpg?nulla=pretium&pede=nisl&ullamcorper=ut&augue=volutpat&a=sapien&suscipit=arcu&nulla=sed&elit=augue&ac=aliquam&nulla=erat&sed=volutpat&vel=in&enim=congue&sit=etiam&amet=justo&nunc=etiam&viverra=pretium&dapibus=iaculis&nulla=justo&suscipit=in&ligula=hac&in=habitasse&lacus=platea&curabitur=dictumst&at=etiam&ipsum=faucibus&ac=cursus&tellus=urna&semper=ut&interdum=tellus&mauris=nulla&ullamcorper=ut&purus=erat&sit=id&amet=mauris&nulla=vulputate&quisque=elementum&arcu=nullam&libero=varius&rutrum=nulla&ac=facilisi&lobortis=cras&vel=non&dapibus=velit&at=nec&diam=nisi&nam=vulputate&tristique=nonummy&tortor=maecenas&eu=tincidunt&pede=lacus",
+            "tags": [
+                "Drama|Fantasy|Thriller",
+                "Horror|Thriller",
+                "Comedy|Horror",
+                "Action|Drama|Thriller"
+            ]
+        },
+        {
+            "id": "2c69297a-1b80-4503-8387-9fd76789a1f2",
+            "title": "Yesterday",
+            "year": 2009,
+            "cover": "http://dummyimage.com/113x115.png/cc0000/ffffff",
+            "description": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+            "contentRating": null,
+            "source": "http://pen.io/eget/eros/elementum/pellentesque/quisque/porta.aspx?commodo=nibh&placerat=in&praesent=hac&blandit=habitasse&nam=platea&nulla=dictumst&integer=aliquam&pede=augue&justo=quam",
+            "tags": [
+                "Action|Comedy|Drama|Musical|Romance",
+                "Musical",
+                "Comedy|Mystery|Romance"
+            ]
+        },
+        {
+            "id": "bb9a14e7-7002-441c-9c80-27b5cffabe74",
+            "title": "Illegal",
+            "year": 2010,
+            "cover": "http://dummyimage.com/104x153.png/5fa2dd/ffffff",
+            "description": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+            "contentRating": null,
+            "source": "https://businessweek.com/vestibulum.js?lectus=nam&vestibulum=dui&quam=proin&sapien=leo&varius=odio&ut=porttitor&blandit=id&non=consequat&interdum=in",
+            "tags": [
+                "Adventure|Drama|War"
+            ]
+        },
+        {
+            "id": "9a2be51a-1f84-41b6-85d0-a2b17915bf09",
+            "title": "Garden of the Finzi-Continis, The (Giardino dei Finzi-Contini, Il)",
+            "year": 1988,
+            "cover": "http://dummyimage.com/199x241.jpg/cc0000/ffffff",
+            "description": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+            "contentRating": null,
+            "source": "https://youtu.be/imperdiet/et/commodo.png?eget=non&eleifend=quam&luctus=nec&ultricies=dui&eu=luctus&nibh=rutrum&quisque=nulla&id=tellus&justo=in&sit=sagittis&amet=dui&sapien=vel&dignissim=nisl&vestibulum=duis&vestibulum=ac&ante=nibh&ipsum=fusce&primis=lacus",
+            "tags": [
+                "Comedy",
+                "Comedy|Fantasy",
+                "Action|Crime|Drama|Thriller"
+            ]
+        },
+        {
+            "id": "770e6687-8165-4591-a3d4-2d73a61da7c6",
+            "title": "Surf Ninjas",
+            "year": 1997,
+            "cover": "http://dummyimage.com/153x189.png/cc0000/ffffff",
+            "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+            "contentRating": null,
+            "source": "https://purevolume.com/mauris/vulputate.js?elementum=tortor&nullam=risus&varius=dapibus&nulla=augue&facilisi=vel&cras=accumsan&non=tellus&velit=nisi&nec=eu&nisi=orci&vulputate=mauris&nonummy=lacinia&maecenas=sapien&tincidunt=quis&lacus=libero&at=nullam&velit=sit&vivamus=amet&vel=turpis&nulla=elementum&eget=ligula&eros=vehicula&elementum=consequat&pellentesque=morbi&quisque=a&porta=ipsum&volutpat=integer&erat=a&quisque=nibh&erat=in&eros=quis&viverra=justo&eget=maecenas&congue=rhoncus&eget=aliquam&semper=lacus&rutrum=morbi&nulla=quis&nunc=tortor&purus=id&phasellus=nulla&in=ultrices&felis=aliquet&donec=maecenas&semper=leo&sapien=odio&a=condimentum&libero=id&nam=luctus&dui=nec&proin=molestie&leo=sed&odio=justo&porttitor=pellentesque&id=viverra&consequat=pede&in=ac&consequat=diam&ut=cras&nulla=pellentesque&sed=volutpat&accumsan=dui&felis=maecenas&ut=tristique&at=est&dolor=et&quis=tempus&odio=semper&consequat=est&varius=quam&integer=pharetra&ac=magna&leo=ac&pellentesque=consequat&ultrices=metus&mattis=sapien&odio=ut&donec=nunc&vitae=vestibulum&nisi=ante&nam=ipsum&ultrices=primis&libero=in&non=faucibus&mattis=orci&pulvinar=luctus&nulla=et&pede=ultrices&ullamcorper=posuere&augue=cubilia&a=curae&suscipit=mauris",
+            "tags": [
+                "Comedy|Drama|Fantasy",
+                "Drama"
+            ]
+        },
+        {
+            "id": "daa76ea5-3cd5-43d5-9726-cceff18c7e4d",
+            "title": "Mystery, Alaska",
+            "year": 2011,
+            "cover": "http://dummyimage.com/142x191.bmp/ff4444/ffffff",
+            "description": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+            "contentRating": null,
+            "source": "https://nsw.gov.au/pharetra.xml?eget=sapien&rutrum=a&at=libero&lorem=nam&integer=dui&tincidunt=proin&ante=leo&vel=odio&ipsum=porttitor&praesent=id&blandit=consequat&lacinia=in&erat=consequat&vestibulum=ut&sed=nulla&magna=sed&at=accumsan&nunc=felis&commodo=ut&placerat=at&praesent=dolor&blandit=quis&nam=odio&nulla=consequat&integer=varius&pede=integer&justo=ac&lacinia=leo&eget=pellentesque&tincidunt=ultrices&eget=mattis&tempus=odio&vel=donec&pede=vitae&morbi=nisi&porttitor=nam&lorem=ultrices&id=libero&ligula=non&suspendisse=mattis&ornare=pulvinar",
+            "tags": [
+                "Comedy|Western",
+                "Comedy|Sci-Fi"
+            ]
+        },
+        {
+            "id": "7212043e-848d-4cc4-9570-b1c5a6629d89",
+            "title": "Octopussy",
+            "year": 2007,
+            "cover": "http://dummyimage.com/234x224.png/5fa2dd/ffffff",
+            "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+            "contentRating": null,
+            "source": "https://instagram.com/justo/morbi/ut/odio/cras/mi/pede.png?etiam=quis&justo=libero&etiam=nullam&pretium=sit&iaculis=amet&justo=turpis&in=elementum&hac=ligula&habitasse=vehicula&platea=consequat&dictumst=morbi&etiam=a",
+            "tags": [
+                "Horror|Thriller",
+                "Drama",
+                "Drama",
+                "Drama|Film-Noir",
+                "Drama"
+            ]
+        },
+        {
+            "id": "108700a1-4fab-4bc8-a635-ecf7da852a24",
+            "title": "Exiled (Fong juk)",
+            "year": 1988,
+            "cover": "http://dummyimage.com/250x141.png/ff4444/ffffff",
+            "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+            "contentRating": null,
+            "source": "https://joomla.org/hac/habitasse/platea/dictumst/maecenas.js?in=pulvinar&hac=sed&habitasse=nisl&platea=nunc&dictumst=rhoncus&aliquam=dui&augue=vel&quam=sem&sollicitudin=sed&vitae=sagittis&consectetuer=nam&eget=congue&rutrum=risus&at=semper&lorem=porta&integer=volutpat&tincidunt=quam&ante=pede&vel=lobortis&ipsum=ligula&praesent=sit&blandit=amet&lacinia=eleifend&erat=pede&vestibulum=libero&sed=quis&magna=orci&at=nullam&nunc=molestie&commodo=nibh&placerat=in&praesent=lectus&blandit=pellentesque&nam=at&nulla=nulla&integer=suspendisse&pede=potenti&justo=cras&lacinia=in&eget=purus&tincidunt=eu&eget=magna&tempus=vulputate&vel=luctus&pede=cum&morbi=sociis&porttitor=natoque",
+            "tags": [
+                "Mystery|Thriller",
+                "Documentary",
+                "Drama"
+            ]
+        }
+    ]
+
+module.exports = { moviesMock }
