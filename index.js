045a183d88980a510f5af70773479ef42cd9be6f
Jairo R <jariver92@gmail.com>
2020-05-02 13:58:39 +0200

    add erro midleware

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
 //Middleware body parser
 app.use(express.json());
 
 moviesApi(app);
 
-app.listen(config.port , function(){
+app.use(logErrors);
+app.use(errorHandler);
+
+app.listen(config.port, function () {
     console.log(`Listening http://localhost:${config.port}`);
 });
