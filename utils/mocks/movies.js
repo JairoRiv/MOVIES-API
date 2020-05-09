71bdd198853d6075678023f1244a42027893be47
Jairo R <jariver92@gmail.com>
2020-05-09 15:51:12 +0200

    add cache control

--- utils/mocks/movies.js
+++ utils/mocks/movies.js
@@ -6,7 +6,8 @@ const moviesMock =
             "year": 2008,
             "cover": "http://dummyimage.com/161x244.jpg/cc0000/ffffff",
             "description": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
-            "contentRating": null,
+            "contentRating": 'G',
+            "duration": 66,
             "source": "http://patch.com/odio/elementum/eu.js?nisi=consequat&eu=dui&orci=nec&mauris=nisi&lacinia=volutpat&sapien=eleifend&quis=donec&libero=ut&nullam=dolor&sit=morbi&amet=vel&turpis=lectus&elementum=in&ligula=quam&vehicula=fringilla&consequat=rhoncus&morbi=mauris&a=enim&ipsum=leo&integer=rhoncus&a=sed&nibh=vestibulum&in=sit&quis=amet&justo=cursus&maecenas=id&rhoncus=turpis&aliquam=integer&lacus=aliquet&morbi=massa&quis=id&tortor=lobortis&id=convallis&nulla=tortor&ultrices=risus&aliquet=dapibus&maecenas=augue&leo=vel&odio=accumsan&condimentum=tellus&id=nisi&luctus=eu&nec=orci&molestie=mauris&sed=lacinia&justo=sapien&pellentesque=quis&viverra=libero&pede=nullam&ac=sit&diam=amet&cras=turpis&pellentesque=elementum&volutpat=ligula&dui=vehicula&maecenas=consequat&tristique=morbi&est=a&et=ipsum&tempus=integer&semper=a&est=nibh&quam=in&pharetra=quis&magna=justo&ac=maecenas&consequat=rhoncus",
             "tags": [
                 "Drama"
@@ -19,6 +20,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/131x153.png/cc0000/ffffff",
             "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
             "contentRating": null,
+            "duration": 96,
             "source": "https://reddit.com/eleifend/donec/ut/dolor.js?ante=eget&ipsum=eros&primis=elementum&in=pellentesque&faucibus=quisque&orci=porta&luctus=volutpat&et=erat&ultrices=quisque&posuere=erat&cubilia=eros&curae=viverra&duis=eget&faucibus=congue&accumsan=eget&odio=semper&curabitur=rutrum&convallis=nulla&duis=nunc&consequat=purus&dui=phasellus&nec=in&nisi=felis&volutpat=donec&eleifend=semper&donec=sapien&ut=a&dolor=libero&morbi=nam&vel=dui&lectus=proin&in=leo&quam=odio&fringilla=porttitor&rhoncus=id&mauris=consequat&enim=in&leo=consequat&rhoncus=ut&sed=nulla&vestibulum=sed&sit=accumsan&amet=felis&cursus=ut&id=at&turpis=dolor&integer=quis&aliquet=odio&massa=consequat&id=varius&lobortis=integer&convallis=ac&tortor=leo&risus=pellentesque&dapibus=ultrices&augue=mattis&vel=odio&accumsan=donec&tellus=vitae&nisi=nisi&eu=nam&orci=ultrices&mauris=libero&lacinia=non&sapien=mattis&quis=pulvinar&libero=nulla&nullam=pede&sit=ullamcorper&amet=augue&turpis=a&elementum=suscipit&ligula=nulla&vehicula=elit&consequat=ac&morbi=nulla&a=sed&ipsum=vel&integer=enim&a=sit&nibh=amet&in=nunc&quis=viverra&justo=dapibus&maecenas=nulla&rhoncus=suscipit&aliquam=ligula&lacus=in&morbi=lacus&quis=curabitur",
             "tags": [
                 "Comedy|Drama",
@@ -33,6 +35,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/240x166.jpg/5fa2dd/ffffff",
             "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
             "contentRating": null,
+            "duration": 56,
             "source": "http://hao123.com/ante/ipsum/primis/in.jpg?nulla=pretium&pede=nisl&ullamcorper=ut&augue=volutpat&a=sapien&suscipit=arcu&nulla=sed&elit=augue&ac=aliquam&nulla=erat&sed=volutpat&vel=in&enim=congue&sit=etiam&amet=justo&nunc=etiam&viverra=pretium&dapibus=iaculis&nulla=justo&suscipit=in&ligula=hac&in=habitasse&lacus=platea&curabitur=dictumst&at=etiam&ipsum=faucibus&ac=cursus&tellus=urna&semper=ut&interdum=tellus&mauris=nulla&ullamcorper=ut&purus=erat&sit=id&amet=mauris&nulla=vulputate&quisque=elementum&arcu=nullam&libero=varius&rutrum=nulla&ac=facilisi&lobortis=cras&vel=non&dapibus=velit&at=nec&diam=nisi&nam=vulputate&tristique=nonummy&tortor=maecenas&eu=tincidunt&pede=lacus",
             "tags": [
                 "Drama|Fantasy|Thriller",
@@ -48,6 +51,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/113x115.png/cc0000/ffffff",
             "description": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
             "contentRating": null,
+            "duration": 80,
             "source": "http://pen.io/eget/eros/elementum/pellentesque/quisque/porta.aspx?commodo=nibh&placerat=in&praesent=hac&blandit=habitasse&nam=platea&nulla=dictumst&integer=aliquam&pede=augue&justo=quam",
             "tags": [
                 "Action|Comedy|Drama|Musical|Romance",
@@ -62,6 +66,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/104x153.png/5fa2dd/ffffff",
             "description": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
             "contentRating": null,
+            "duration": 66,
             "source": "https://businessweek.com/vestibulum.js?lectus=nam&vestibulum=dui&quam=proin&sapien=leo&varius=odio&ut=porttitor&blandit=id&non=consequat&interdum=in",
             "tags": [
                 "Adventure|Drama|War"
@@ -74,6 +79,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/199x241.jpg/cc0000/ffffff",
             "description": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
             "contentRating": null,
+            "duration": 36,
             "source": "https://youtu.be/imperdiet/et/commodo.png?eget=non&eleifend=quam&luctus=nec&ultricies=dui&eu=luctus&nibh=rutrum&quisque=nulla&id=tellus&justo=in&sit=sagittis&amet=dui&sapien=vel&dignissim=nisl&vestibulum=duis&vestibulum=ac&ante=nibh&ipsum=fusce&primis=lacus",
             "tags": [
                 "Comedy",
@@ -88,6 +94,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/153x189.png/cc0000/ffffff",
             "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
             "contentRating": null,
+            "duration": 120,
             "source": "https://purevolume.com/mauris/vulputate.js?elementum=tortor&nullam=risus&varius=dapibus&nulla=augue&facilisi=vel&cras=accumsan&non=tellus&velit=nisi&nec=eu&nisi=orci&vulputate=mauris&nonummy=lacinia&maecenas=sapien&tincidunt=quis&lacus=libero&at=nullam&velit=sit&vivamus=amet&vel=turpis&nulla=elementum&eget=ligula&eros=vehicula&elementum=consequat&pellentesque=morbi&quisque=a&porta=ipsum&volutpat=integer&erat=a&quisque=nibh&erat=in&eros=quis&viverra=justo&eget=maecenas&congue=rhoncus&eget=aliquam&semper=lacus&rutrum=morbi&nulla=quis&nunc=tortor&purus=id&phasellus=nulla&in=ultrices&felis=aliquet&donec=maecenas&semper=leo&sapien=odio&a=condimentum&libero=id&nam=luctus&dui=nec&proin=molestie&leo=sed&odio=justo&porttitor=pellentesque&id=viverra&consequat=pede&in=ac&consequat=diam&ut=cras&nulla=pellentesque&sed=volutpat&accumsan=dui&felis=maecenas&ut=tristique&at=est&dolor=et&quis=tempus&odio=semper&consequat=est&varius=quam&integer=pharetra&ac=magna&leo=ac&pellentesque=consequat&ultrices=metus&mattis=sapien&odio=ut&donec=nunc&vitae=vestibulum&nisi=ante&nam=ipsum&ultrices=primis&libero=in&non=faucibus&mattis=orci&pulvinar=luctus&nulla=et&pede=ultrices&ullamcorper=posuere&augue=cubilia&a=curae&suscipit=mauris",
             "tags": [
                 "Comedy|Drama|Fantasy",
@@ -101,6 +108,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/142x191.bmp/ff4444/ffffff",
             "description": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
             "contentRating": null,
+            "duration": 66,
             "source": "https://nsw.gov.au/pharetra.xml?eget=sapien&rutrum=a&at=libero&lorem=nam&integer=dui&tincidunt=proin&ante=leo&vel=odio&ipsum=porttitor&praesent=id&blandit=consequat&lacinia=in&erat=consequat&vestibulum=ut&sed=nulla&magna=sed&at=accumsan&nunc=felis&commodo=ut&placerat=at&praesent=dolor&blandit=quis&nam=odio&nulla=consequat&integer=varius&pede=integer&justo=ac&lacinia=leo&eget=pellentesque&tincidunt=ultrices&eget=mattis&tempus=odio&vel=donec&pede=vitae&morbi=nisi&porttitor=nam&lorem=ultrices&id=libero&ligula=non&suspendisse=mattis&ornare=pulvinar",
             "tags": [
                 "Comedy|Western",
@@ -114,6 +122,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/234x224.png/5fa2dd/ffffff",
             "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
             "contentRating": null,
+            "duration": 66,
             "source": "https://instagram.com/justo/morbi/ut/odio/cras/mi/pede.png?etiam=quis&justo=libero&etiam=nullam&pretium=sit&iaculis=amet&justo=turpis&in=elementum&hac=ligula&habitasse=vehicula&platea=consequat&dictumst=morbi&etiam=a",
             "tags": [
                 "Horror|Thriller",
@@ -130,6 +139,7 @@ const moviesMock =
             "cover": "http://dummyimage.com/250x141.png/ff4444/ffffff",
             "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
             "contentRating": null,
+            "duration": 66,
             "source": "https://joomla.org/hac/habitasse/platea/dictumst/maecenas.js?in=pulvinar&hac=sed&habitasse=nisl&platea=nunc&dictumst=rhoncus&aliquam=dui&augue=vel&quam=sem&sollicitudin=sed&vitae=sagittis&consectetuer=nam&eget=congue&rutrum=risus&at=semper&lorem=porta&integer=volutpat&tincidunt=quam&ante=pede&vel=lobortis&ipsum=ligula&praesent=sit&blandit=amet&lacinia=eleifend&erat=pede&vestibulum=libero&sed=quis&magna=orci&at=nullam&nunc=molestie&commodo=nibh&placerat=in&praesent=lectus&blandit=pellentesque&nam=at&nulla=nulla&integer=suspendisse&pede=potenti&justo=cras&lacinia=in&eget=purus&tincidunt=eu&eget=magna&tempus=vulputate&vel=luctus&pede=cum&morbi=sociis&porttitor=natoque",
             "tags": [
                 "Mystery|Thriller",
@@ -139,4 +149,22 @@ const moviesMock =
         }
     ]
 
-module.exports = { moviesMock }
+function filterMoviesMock(tag) {
+    return moviesMock.filter(movie => movie.tags.includes(tag));
+}
+
+class MoviesServiceMock {
+    async getMovie() {
+        return Promise.resolve(moviesMock);
+    };
+
+    async createMovie() {
+        return Promise.resolve(moviesMock[0]);
+    };
+}
+
+module.exports = {
+    moviesMock,
+    filterMoviesMock,
+    MoviesServiceMock
+};
